b506ad111c83f54624401391085b0681a3e007f2
Grzegorz Bogusz <grzegorz.bogusz@creativestyle.pl>
2020-02-11 09:32:40 +0000

    [KRG-517] Set dhl_customer_number in order

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -7,9 +7,9 @@ var config = {
             'Magento_Checkout/js/model/quote': {
                 'MageSuite_PackstationDhl/js/model/quote-ext': true,
             },
-            // 'Magento_Checkout/js/view/shipping-information': {
-            //     'MageSuite_PackstationDhl/js/view/shipping-information-ext': true,
-            // },
+            'Magento_Checkout/js/view/shipping-information': {
+                'MageSuite_PackstationDhl/js/view/shipping-information-ext': true,
+            },
         },
     },
 };
